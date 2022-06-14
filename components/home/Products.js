cf5269f6fd70c3870e005caa17c3573630383164
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-06-14 11:28:40 +0200

    basic homepage products layout

--- components/home/Products.js
+++ components/home/Products.js
@@ -1,21 +1,57 @@
 import { useState, useEffect, useContext } from 'react';
+import Image from 'next/image';
 import ProductContext from '../../context/ProductContext';
-import axios from 'axios';
+import CategoryContext from '../../context/CategoryContext';
 
 function Products() {
+  const { categories } = useContext(CategoryContext);
   const { products } = useContext(ProductContext);
 
+  const drinksProducts = products.filter(
+    (product) => product.category === 'Drinks'
+  );
+
   return (
-    <>
-      <h2>Products</h2>
-      {products.map((product) => {
+    <section className=' mt-6'>
+      {categories.map((category) => {
         return (
-          <div key={product._id}>
-            <h3>{product.name}</h3>
+          <div className='mb-6' key={category._id}>
+            <div className=' w-[200px] h-auto mx-auto'>
+              <Image
+                src={`/uploads/${category.image}`}
+                alt={category.name}
+                width={100}
+                height={100}
+                layout='responsive'
+                objectFit='contain'
+                priority={true}
+              />
+            </div>
+            {/* <h3>{category.name}</h3> */}
+            <ul className='flex flex-col gap-4'>
+              {products.map((product) => {
+                return (
+                  <li key={product._id}>
+                    {product.category === category.name && (
+                      <div className='flex items-center justify-between cursor-pointer border-2'>
+                        <div className='flex gap-4'>
+                          <h3 className=' font-bold'>{product.name}</h3>
+                          <p>{product.description}</p>
+                        </div>
+                        <div className='flex gap-4'>
+                          <p>{product.category}</p>
+                          <p className=' font-bold'>&euro; {product.price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         );
       })}
-    </>
+    </section>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,8 @@ import Products from '../components/home/Products';
 
 export default function Home() {
   return (
-    <div className=''>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <>
       <Products />
-    </div>
+    </>
   );
 }
