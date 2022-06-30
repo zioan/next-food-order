25f4f5c80c000057d3adff9ab5160433d50a9675
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-06-30 08:59:17 +0200

    fix next js serverSideProps bug in user profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,26 +1,38 @@
+import { useEffect } from 'react';
 import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import UserProfile from '../components/profile/UserProfile';
 
 function ProfilePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        router.replace('/');
+      }
+    });
+  }, [router]);
+
   return <UserProfile />;
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession({ req: context.req });
+// export async function getServerSideProps(context) {
+//   const session = await getSession({ req: context.req });
 
-  if (!session) {
-    return {
-      redirect: {
-        // notFound: true //404 page
-        destination: '/auth',
-        permanent: false,
-      },
-    };
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         // notFound: true //404 page
+//         destination: '/auth',
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: { session },
-  };
-}
+//   return {
+//     props: { session },
+//   };
+// }
 
 export default ProfilePage;
